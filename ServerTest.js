2f878bc1cc52c91207ba920d48a921be2f1c45c2
Lien <Lien@192.168.0.80>
2018-01-27 17:18:49 +0100

    Upload

---
+++ ServerTest.js
@@ -0,0 +1,42 @@
+"use strict";
+// Node-Http-Modul importieren
+const Http = require("http");
+// Node-Url-Modul importieren
+const Url = require("url");
+var ServerTest;
+(function (ServerTest) {
+    // Port vom Process-Objekt erfragen 
+    let port = process.env.PORT;
+    // Port nicht definiert -> lokale Maschine, Port selbst definieren
+    if (port == undefined)
+        port = 8100;
+    // Server-Objekt kreieren
+    let server = Http.createServer();
+    // Event-Handler installieren
+    server.addListener("listening", handleListen);
+    server.addListener("request", handleRequest);
+    // Auf dem Port horchen
+    server.listen(port);
+    // Listening-Event: Rückmeldung wenn horchen läuft
+    function handleListen() {
+        console.log("Server listening on port " + port);
+    }
+    // Request-Event: Verarbeiten der Request und erstellen der Response
+    function handleRequest(_request, _response) {
+        console.log("Vielen Dank für Ihre Bestellung");
+        // Header: Antwort kommt im HTML-Format mit uft-8
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        // Header: ?
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        // Response-Body
+        _response.write("Vielen Dank für Ihre Bestellung!!<br>");
+        // ?
+        let query = Url.parse(_request.url, true).query;
+        // ?
+        for (let key in query)
+            _response.write(key + ": " + query[key] + "<br>");
+        // Antwort abschließen und abschicken
+        _response.end();
+    }
+})(ServerTest || (ServerTest = {}));
+//# sourceMappingURL=ServerTest.js.map
---
+++ ServerTest.ts
@@ -0,0 +1,51 @@
+// Node-Http-Modul importieren
+import * as Http from "http";
+// Node-Url-Modul importieren
+import * as Url from "url"; 
+
+namespace ServerTest {
+    // Neuer Datentyp AssocStringString: homogenes, assoziatives Array.
+    interface AssocStringString {
+        [key: string]: string;  
+    }
+    
+    // Port vom Process-Objekt erfragen 
+    let port: number = process.env.PORT;
+    // Port nicht definiert -> lokale Maschine, Port selbst definieren
+    if (port == undefined)
+        port = 8100;
+    
+    // Server-Objekt kreieren
+    let server: Http.Server = Http.createServer();
+    // Event-Handler installieren
+    server.addListener("listening", handleListen);
+    server.addListener("request", handleRequest);
+    // Auf dem Port horchen
+    server.listen(port);
+
+    // Listening-Event: Rückmeldung wenn horchen läuft
+    function handleListen(): void {
+        console.log("Server listening on port " + port);
+    }
+
+    // Request-Event: Verarbeiten der Request und erstellen der Response
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        console.log("Vielen Dank für Ihre Bestellung");
+        // Header: Antwort kommt im HTML-Format mit uft-8
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        // Header: ?
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        
+        // Response-Body
+        _response.write("Vielen Dank für Ihre Bestellung!!<br>");
+
+        // ?
+        let query: AssocStringString = Url.parse(_request.url, true).query;
+        // ?
+        for (let key in query)
+            _response.write(key + ": " + query[key] +"<br>");
+        
+        // Antwort abschließen und abschicken
+        _response.end();
+    }
+}
